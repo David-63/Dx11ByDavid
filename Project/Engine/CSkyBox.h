6f9a1d48f68d5a9a68dd540d890cbead95c34502
Jiseop63 <jiseop.63@gmail.com>
2023-12-21 14:00:55 +0900

    3D 렌더링 추가

---
+++ Project/Engine/CSkyBox.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "CRenderComp.h"
+
+enum class SKYBOX_TYPE
+{
+    SPHERE,
+    CUBE,
+};
+
+class CSkyBox : public CRenderComp
+{
+private:
+    SKYBOX_TYPE     m_Type;
+    Ptr<CTexture>   m_SkyBoxTex;
+
+public:
+    void SetSkyBoxType(SKYBOX_TYPE _Type);
+    void SetSkyBoxTexture(Ptr<CTexture> _Tex);
+
+public:
+    virtual void finaltick() override;
+    virtual void render() override;
+
+    CLONE(CSkyBox);
+public:
+    CSkyBox();
+    ~CSkyBox();
+};
+
